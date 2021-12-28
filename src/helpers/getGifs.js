508811cd127067451dab331650bca0722f218ab3
SadgamS <brayan010.ba@gmail.com>
2021-12-27 21:07:14 -0400

    first commit

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,15 @@
+export const getGifs = async(category) => {
+    const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=tFYRs6Kjjn2LqcD79OXv3hR85YslU2CH`
+    const resp = await fetch(url);
+    const {data} = await resp.json();
+
+    const gifs = data.map( img => {
+        return {
+            id: img.id,
+            title: img.title,
+            url: img.images?.downsized_medium.url
+        }
+    })
+
+    return gifs;
+}
